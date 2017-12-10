c3de392ac1f3f057e855ce96079d53e3ad6d82ce
Reza Farhad <reza.f@mac.com>
2017-12-10 19:21:18 +0000

    Initial Commit

---
+++ DrawSpeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrawSpeed
+//
+//  Created by Reza Farhad on 10/12/2017.
+//  Copyright © 2017 Reza Farhad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
